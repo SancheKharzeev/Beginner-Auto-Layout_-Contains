e9488d5d0685c6665c03b95f22710c11d762b27d
Aleksandr Kh <akumasanta@gmail.com>
2023-03-19 18:29:51 +0600

    Initial Commit

---
+++ Beginner Auto Layout_ Contains/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Beginner Auto Layout_ Contains
+//
+//  Created by Александр Х on 19.03.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Beginner Auto Layout_ ContainsTests/Beginner_Auto_Layout__ContainsTests.swift
@@ -0,0 +1,36 @@
+//
+//  Beginner_Auto_Layout__ContainsTests.swift
+//  Beginner Auto Layout_ ContainsTests
+//
+//  Created by Александр Х on 19.03.2023.
+//
+
+import XCTest
+@testable import Beginner_Auto_Layout__Contains
+
+final class Beginner_Auto_Layout__ContainsTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        // Any test you write for XCTest can be annotated as throws and async.
+        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
+        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
